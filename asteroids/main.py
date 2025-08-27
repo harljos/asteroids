43fb10b409d8c960a859c08ab48de66e08cf0c6c
unknown <joseph.harley02@gmail.com>
2025-08-27 17:12:14 -0400

    added player objects into groups

--- asteroids/main.py
+++ asteroids/main.py
@@ -9,6 +9,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -16,9 +19,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
