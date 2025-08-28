576b1f7f778f7c9fa3911a8e3007e58c5f1123ee
unknown <joseph.harley02@gmail.com>
2025-08-28 09:35:53 -0400

    added shoot functionality to player

--- asteroids/main.py
+++ asteroids/main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -22,6 +23,9 @@ def main():
 
     AsteroidField.containers = updatable
     AsteroidField()
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
 
     while True:
         for event in pygame.event.get():
